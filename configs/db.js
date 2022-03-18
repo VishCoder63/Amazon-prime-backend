2448cf80a7604efceb45112bf2bfdd7fdff3ca36
Vishnu Nair <nvishnu1995@gmail.com>
2022-03-18 14:10:54 +0530

    sorting auth issue

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 module.exports = () => {
   let connection_String = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3r8xl.mongodb.net/PrimeVideo?retryWrites=true&w=majority`;
-  // let connection_String = `mongodb+srv://vish95:vish95@cluster0.3r8xl.mongodb.net/PrimeVideo?retryWrites=true&w=majority`;
   // mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w1a4o.mongodb.net/pepperfryDB?retryWrites=true&w=majority`)
   mongoose.connect(connection_String);
 };
