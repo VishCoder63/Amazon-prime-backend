2a9799f3616f67fe9af28bcbb3d0e7cf5ba14dd8
Vishnu Nair <nvishnu1995@gmail.com>
2022-03-18 13:27:18 +0530

    clear

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-module.exports = () => {  
-  let connection_String=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3r8xl.mongodb.net/PrimeVideo?retryWrites=true&w=majority`
+require("dotenv").config();
+const mongoose = require("mongoose");
+console.log("user:", process.env.DB_USER);
+module.exports = () => {
+  let connection_String = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3r8xl.mongodb.net/PrimeVideo?retryWrites=true&w=majority`;
   // mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w1a4o.mongodb.net/pepperfryDB?retryWrites=true&w=majority`)
-  mongoose.connect(connection_String)
-}
+  mongoose.connect(connection_String);
+};
